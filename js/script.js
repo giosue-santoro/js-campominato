d675a0191db08af93de3ef8695c853ee022d46d9
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-08 17:44:37 +0100

    esercizio funzionante , funzioni da aggiungere per migliorare leggibilità

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ for(var i = 0; containerMina.length < 16; i++){
     if(containerMina.indexOf(random) === -1) containerMina.push(random);
 }
 
+console.log(containerMina);
+
 //Creo while loop che chiede all'utente di inserire un numero tra 1 e 100 fino a che non trova un numero contenuto in containerMina
 //L'utente viene obbligato a scegliere un integer compreso tra 1 e 100
 
@@ -18,17 +20,18 @@ while(!containerMina.includes(userNumber)){
     var userNumber = parseInt(prompt('Scegli un numero compreso tra 1 e 100'));
     if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
         alert('Puoi scegliere solo un numero intero compreso tra 1 e 100');
-    }
+    } else {
+        userNumber = parseInt(userNumber);
+        if (containerUser.indexOf(userNumber) === -1) { //Se il numero scelto non è ancora nell'array containerUser, pusha il numero scelto dall'utente nell'array
+            containerUser.push(userNumber)
+            console.log(userNumber);
+        }
+    } 
 }
 
-
-
-
-
-
-
-
-
-
+//Notifica utente che ha perso la partita
+if (containerMina.includes(userNumber)) {
+    alert('Sei esploso');
+}
 
 
