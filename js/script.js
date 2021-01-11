e2f8c3a277c4cbbe87db723bfb9f39e791aafd67
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-11 16:15:30 +0100

    da sistemare secondo ciclo per includere difficoltà scelta

--- js/script.js
+++ js/script.js
@@ -1,37 +1,62 @@
+//Funzioni
+
+function randomNumber(min, max) {
+    var result = Math.floor(Math.random() * (max + 1 - min) + min);
+    return result;
+  }
+
 //Variabili generali
 
 var containerMina = [];
 var containerUser = [];
 var userNumber;
+var numeroMine;
 
-//Creo array dentro cui verranno pushati i 16 numeri random tra 1 e 100 (che non potranno essere non uguali tra di loro)
+//Faccio scegliere all'utente il livello
+level = prompt('Scegli il livello tra facile, intermedio e difficile');
 
-for(var i = 0; containerMina.length < 16; i++){
-    var random = Math.floor(Math.random() * 100) + 1;
-    if(containerMina.indexOf(random) === -1) containerMina.push(random);
-}
+switch (level) {
+    case 'facile':
+      alert('Hai scelto la modalità facile');
+      numeroMine = 100;
+      break;
+    case 'intemedia':
+      alert('Hai scelto la modalità intermedia');
+      numeroMine = 80;
+      break;
+    case  'difficile':
+      alert('Hai scelto la modalità difficile');
+      numeroMine = 50;
+      break;  
+    }
 
-console.log(containerMina);
+//Creo array dentro cui verranno pushati i numeri in base alla difficoltà scelta random tra 1 e 100 (che non potranno essere non uguali tra di loro)
+    for(var i = 0; containerMina.length < 16; i++){
+        var random = randomNumber(1, numeroMine)
+        if(containerMina.indexOf(random) === -1) containerMina.push(random);
+    }
 
-//Creo while loop che chiede all'utente di inserire un numero tra 1 e 100 fino a che non trova un numero contenuto in containerMina
-//L'utente viene obbligato a scegliere un integer compreso tra 1 e 100
+    console.log(containerMina);
 
-while(!containerMina.includes(userNumber)){
-    var userNumber = parseInt(prompt('Scegli un numero compreso tra 1 e 100'));
-    if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
-        alert('Puoi scegliere solo un numero intero compreso tra 1 e 100');
-    } else {
-        userNumber = parseInt(userNumber);
-        if (containerUser.indexOf(userNumber) === -1) { //Se il numero scelto non è ancora nell'array containerUser, pusha il numero scelto dall'utente nell'array
-            containerUser.push(userNumber)
-            console.log(userNumber);
-        }
-    } 
-}
+    //Creo while loop che chiede all'utente di inserire un numero tra 1 e 100 fino a che non trova un numero contenuto in containerMina
+    //L'utente viene obbligato a scegliere un integer compreso tra 1 e 100
 
-//Notifica utente che ha perso la partita
-if (containerMina.includes(userNumber)) {
-    alert('Sei esploso');
-}
+    while(!containerMina.includes(userNumber)){
+        var userNumber = parseInt(prompt('Scegli un numero compreso tra 1 e 100'));
+        if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+            alert('Puoi scegliere solo un numero intero compreso tra 1 e 100');
+        } else {
+            userNumber = parseInt(userNumber);
+            if (containerUser.indexOf(userNumber) === -1) { //Se il numero scelto non è ancora nell'array containerUser, pusha il numero scelto dall'utente nell'array
+                containerUser.push(userNumber)
+                console.log(userNumber);
+            }
+        } 
+    }
+
+    //Notifica utente che ha perso la partita
+    if (containerMina.includes(userNumber)) {
+        alert('Sei esploso');
+    }
 
 
